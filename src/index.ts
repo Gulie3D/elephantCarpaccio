de2b6dd67f007e8dff7ad8683b6e93b3febbb8a0
Gulie3D <julie-causse@live.fr>
2025-01-10 15:47:13 +0100

    Tranche 4 : Calculer et afficher le prix total

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,12 @@ console.log("1000€"); //tache 2
 console.log("Quantité : 5"); // tache 3
 console.log("Prix : 100 €");
 
+// tache 4 
+const quantity = 5;
+const price = 100;
+const total = quantity * price;
+
+console.log(`Quantité : ${quantity}`);
+console.log(`Prix : ${price} €`);
+console.log(`Total : ${total} €`);
+
